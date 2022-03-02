787e00835972a7f32f7cbf7f61661ba9b1989a61
Daniel <danlpsarcanjo@gmail.com>
2022-03-01 23:05:05 -0500

    Create users resolver

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,42 @@
+import { NotFoundException } from '@nestjs/common';
+import { Args, Field, InputType, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { assign } from 'src/utils/assign';
+import { User } from './users.model';
+
+@InputType()
+class CreateUserInput {
+  @Field()
+  name!: string;
+
+  @Field()
+  age!: number;
+}
+
+const users: User[] = [];
+
+@Resolver(() => User)
+export class UsersResolver {
+  @Mutation(() => User)
+  async createUser(@Args('input') input: CreateUserInput): Promise<User> {
+    const user = assign(new User(), input, {
+      id: users.length + 1,
+    });
+
+    users.push(user);
+    return users[users.length - 1];
+  }
+
+  @Query(() => [User])
+  async getUsers(): Promise<User[]> {
+    return users;
+  }
+
+  @Query(() => User)
+  async getUser(@Args('id', { type: () => Int }) id: number): Promise<User> {
+    const user = users[id - 1];
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+}
