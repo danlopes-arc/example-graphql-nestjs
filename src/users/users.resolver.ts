33bc5affc65d18826feb1d9997b1b11adb0511d6
Daniel <danlpsarcanjo@gmail.com>
2022-03-03 21:59:04 -0500

    Add error when name exists

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Args, Field, InputType, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { assign } from 'src/utils/assign';
 import { User } from './users.model';
@@ -18,6 +18,12 @@ const users: User[] = [];
 export class UsersResolver {
   @Mutation(() => User)
   async createUser(@Args('input') input: CreateUserInput): Promise<User> {
+    const existingUser = users.find((user) => user.name === input.name);
+
+    if (existingUser) {
+      throw new BadRequestException({ validationErrors: { name: 'Name already exists.' } });
+    }
+
     const user = assign(new User(), input, {
       id: users.length + 1,
     });
